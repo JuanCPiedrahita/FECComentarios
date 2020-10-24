6758d6b24ecce231604c7755cc0b1a5be0b481c8
Juan Camilo Piedrahita Londoño <jcpiedra@bancolombia.com.co>
2020-10-24 17:04:13 -0500

    Aprendiendo Angular

---
+++ src/app/components/agregar-editar-comentario/agregar-editar-comentario.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-agregar-editar-comentario',
+  templateUrl: './agregar-editar-comentario.component.html',
+  styleUrls: ['./agregar-editar-comentario.component.css']
+})
+export class AgregarEditarComentarioComponent implements OnInit {
+  comentarios: FormGroup;
+
+  constructor(private fb: FormBuilder) { 
+    this.comentarios = this.fb.group({
+      titulo: ['', Validators.required],
+      creador: ['', Validators.required],
+      texto: ['', Validators.required],
+
+    });
+  }
+
+  ngOnInit(): void {
+  }
+guardarComentario(){
+  console.log(this.comentarios);
+}
+}
